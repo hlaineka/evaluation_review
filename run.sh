852b34b134e455cda75a0d08dcc4ebdca37f393b
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-04-27 09:33:33 +0300

    fix2 to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-sudo docker image rm -f eval
-sudo docker build -t eval .
 if test ! -f "src/config.yml"; then
 	mv src/config_example.yml src/config.yml
 fi
+sudo docker image rm -f eval
+sudo docker build -t eval .
 xdg-open "http://localhost:6660/"
 sudo docker run -p 6660:6660 -t -i eval
