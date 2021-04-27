972fffbca31c2fcd6c0088bc7ca682ead24ee128
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-04-27 09:26:15 +0300

    fix to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 sudo docker image rm -f eval
 sudo docker build -t eval .
-if test ! -f "config.yml"; then
-	mv config_example.yml config.yml
+if test ! -f "src/config.yml"; then
+	mv src/config_example.yml src/config.yml
 fi
 xdg-open "http://localhost:6660/"
 sudo docker run -p 6660:6660 -t -i eval
